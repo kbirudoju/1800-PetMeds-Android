8313f60ec080113c64c89e6141bf0d9dee1b32aa
ssrivastava-dminc-com <ssrivastava@dminc.com>
2016-10-04 17:00:53 +0530

    [1-800-PetMeds] Added GIF For loading in background with Transparent Alpha Change-Id: I3ad1b803dccc64726f61c4b18d2e695dd620b0a7

--- app/src/main/java/com/petmeds1800/ui/AbstractActivity.java
+++ app/src/main/java/com/petmeds1800/ui/AbstractActivity.java
@@ -1,14 +1,20 @@
 package com.petmeds1800.ui;
 
+import com.bumptech.glide.Glide;
 import com.petmeds1800.R;
+import com.petmeds1800.ui.fragments.dialog.LoadingGIFDialogFragment;
 import com.petmeds1800.util.AnalyticsUtil;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -73,4 +79,17 @@ public abstract class AbstractActivity extends AppCompatActivity {
         return mToolbar;
     }
 
+    public void startLoadingGif(Context mContext) throws Exception{
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        LoadingGIFDialogFragment frag = new LoadingGIFDialogFragment();
+        frag.show(fragmentTransaction, LoadingGIFDialogFragment.class.getSimpleName());
+    }
+
+    public void stopLoadingGif(Context mContext) throws Exception{
+        Fragment prev = getSupportFragmentManager().findFragmentByTag(LoadingGIFDialogFragment.class.getSimpleName());
+        if (prev != null) {
+            LoadingGIFDialogFragment df = (LoadingGIFDialogFragment) prev;
+            df.dismiss();
+        }
+    }
 }
---
+++ app/src/main/java/com/petmeds1800/ui/fragments/dialog/LoadingGIFDialogFragment.java
@@ -0,0 +1,51 @@
+package com.petmeds1800.ui.fragments.dialog;
+
+import android.app.Dialog;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.petmeds1800.R;
+
+import butterknife.BindView;
+
+/**
+ * Created by Sarthak on 10/4/2016.
+ */
+
+public class LoadingGIFDialogFragment extends DialogFragment {
+
+    ImageView mLoadingGif;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.MY_DIALOG);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog d = getDialog();
+        if (d!=null){
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            d.getWindow().setLayout(width, height);
+
+            Glide.with(getActivity()).load(Uri.parse("file:///android_asset/Loading.gif")).asGif().crossFade().into(mLoadingGif);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_loading_gif, container, false);
+        mLoadingGif = (ImageView) root.findViewById(R.id.loading_gif_image);
+        return root;
+    }
+}
