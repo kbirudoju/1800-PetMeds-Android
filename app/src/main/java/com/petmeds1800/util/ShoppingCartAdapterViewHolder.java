414dce30f00ec20fec9e544ca23ba1f8168ebefa
ssrivastava-dminc-com <ssrivastava@dminc.com>
2016-10-04 15:31:00 +0530

    [1-800-PetMeds] DEV-ANDROID-457 | Cart(2nd) Implementation https://jira.dminc-gtc.com/browse/PET-672#

--- app/src/main/java/com/petmeds1800/util/ShoppingCartAdapterViewHolder.java
+++ app/src/main/java/com/petmeds1800/util/ShoppingCartAdapterViewHolder.java
@@ -3,9 +3,9 @@ package com.petmeds1800.util;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.petmeds1800.R;
@@ -17,24 +17,26 @@ import com.petmeds1800.R;
 public class ShoppingCartAdapterViewHolder extends RecyclerView.ViewHolder {
 
     ImageView itemImage;
-    TextView itemTitle;
-    TextView itemDescription;
-    TextInputLayout itemQuantityDescription;
-    TextView itemCost;
-    FrameLayout editItem;
-    FrameLayout deleteItem;
+    TextView mItemTitle;
+    TextView mItemDescription;
+    TextInputLayout mItemQuantityDescription;
+    TextView mItemCost;
+    FrameLayout mEditItemLayout;
+    FrameLayout mDeleteItemLayout;
+    Spinner mItemQuantitySpinner;
 
     public ShoppingCartAdapterViewHolder(View v, int TYPE) {
         super(v);
 
         if (TYPE == ShoppingCartRecyclerViewAdapter.TYPE_ITEM){
             itemImage = (ImageView) v.findViewById(R.id.item_main_image);
-            itemTitle = (TextView) v.findViewById(R.id.item_main_title);
-            itemDescription = (TextView) v.findViewById(R.id.item_main_desc);
-            itemQuantityDescription = (TextInputLayout) v.findViewById(R.id.item_quantity_input_layout);
-            itemCost = (TextView) v.findViewById(R.id.each_item_selling_price);
-            editItem = (FrameLayout) v.findViewById(R.id.button_edit);
-            deleteItem = (FrameLayout) v.findViewById(R.id.button_remove);
+            mItemTitle = (TextView) v.findViewById(R.id.item_main_title);
+            mItemDescription = (TextView) v.findViewById(R.id.item_main_desc);
+            mItemQuantityDescription = (TextInputLayout) v.findViewById(R.id.item_quantity_input_layout);
+            mItemCost = (TextView) v.findViewById(R.id.each_item_selling_price);
+            mEditItemLayout = (FrameLayout) v.findViewById(R.id.button_edit);
+            mDeleteItemLayout = (FrameLayout) v.findViewById(R.id.button_remove);
+            mItemQuantitySpinner = (Spinner) v.findViewById(R.id.spinner_item_quantity);
         }
     }
 }
--- app/src/main/java/com/petmeds1800/util/ShoppingCartRecyclerViewAdapter.java
+++ app/src/main/java/com/petmeds1800/util/ShoppingCartRecyclerViewAdapter.java
@@ -5,13 +5,18 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.os.RemoteException;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.ViewGroup;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
@@ -31,14 +36,14 @@ public class ShoppingCartRecyclerViewAdapter extends RecyclerView.Adapter<Shoppi
     public static final int TYPE_FOOTER = 0;
     public static final int TYPE_ITEM = 1;
 
-    private ArrayList<CommerceItems> commerceItemsesCollection;
-    private View itemFooter;
+    private ArrayList<CommerceItems> mCommerceItemsesCollection;
+    private View mItemFooter;
     private Context mContext;
     private Handler mHandler;
 
     public ShoppingCartRecyclerViewAdapter(ArrayList<CommerceItems> commerceItemsesCollection, View itemFooter, Context context, Handler handler) {
-        this.commerceItemsesCollection = commerceItemsesCollection;
-        this.itemFooter = itemFooter;
+        this.mCommerceItemsesCollection = commerceItemsesCollection;
+        this.mItemFooter = itemFooter;
         this.mContext = context;
         this.mHandler = handler;
     }
@@ -50,7 +55,7 @@ public class ShoppingCartRecyclerViewAdapter extends RecyclerView.Adapter<Shoppi
         if (viewType == TYPE_ITEM) {
             searchItemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.cart_each_item_layout, parent, false);
         } else if (viewType == TYPE_FOOTER) {
-            searchItemView = itemFooter;
+            searchItemView = mItemFooter;
         }
         return new ShoppingCartAdapterViewHolder(searchItemView,viewType);
     }
@@ -62,12 +67,12 @@ public class ShoppingCartRecyclerViewAdapter extends RecyclerView.Adapter<Shoppi
             return;
         }
 
-        holder.itemTitle.setText(commerceItemsesCollection.get(position).getProductDisplayName());
-        holder.itemDescription.setText(commerceItemsesCollection.get(position).getSkuDisplayName());
-        holder.itemQuantityDescription.getEditText().setText(commerceItemsesCollection.get(position).getQuantity());
-        holder.itemCost.setText(Float.toString(commerceItemsesCollection.get(position).getSellingPrice()));
+        holder.mItemTitle.setText(mCommerceItemsesCollection.get(position).getProductDisplayName());
+        holder.mItemDescription.setText(mCommerceItemsesCollection.get(position).getSkuDisplayName());
 
-        Glide.with(mContext).load(mContext.getString(R.string.server_endpoint) + commerceItemsesCollection.get(position).getImageUrl()).asBitmap().centerCrop().into(new BitmapImageViewTarget(holder.itemImage) {
+        holder.mItemCost.setText(Float.toString(mCommerceItemsesCollection.get(position).getSellingPrice()));
+
+        Glide.with(mContext).load(mContext.getString(R.string.server_endpoint) + mCommerceItemsesCollection.get(position).getImageUrl()).asBitmap().centerCrop().into(new BitmapImageViewTarget(holder.itemImage) {
 
             @Override
             protected void setResource(Bitmap resource) {
@@ -77,13 +82,13 @@ public class ShoppingCartRecyclerViewAdapter extends RecyclerView.Adapter<Shoppi
             }
         });
 
-        holder.deleteItem.setOnClickListener(new View.OnClickListener() {
+        holder.mDeleteItemLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Message msg = Message.obtain(null,Constants.DELETE_ITEM_REQUEST_SHOPPINGCART);
                 Bundle b = new Bundle();
-                b.putString(Constants.COMMERCE_ITEM_ID,commerceItemsesCollection.get(position).getCommerceItemId());
+                b.putString(Constants.COMMERCE_ITEM_ID, mCommerceItemsesCollection.get(position).getCommerceItemId());
                 msg.setData(b);
 
                 try {
@@ -96,36 +101,196 @@ public class ShoppingCartRecyclerViewAdapter extends RecyclerView.Adapter<Shoppi
             }
         });
 
-        holder.editItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        if (true == mCommerceItemsesCollection.get(position).isRxItem()) {
 
-                Message msg = Message.obtain(null,Constants.CLICK_ITEM_UPDATE_SHOPPINGCART);
-                Bundle b = new Bundle();
-                b.putString(CommonWebviewFragment.TITLE_KEY,commerceItemsesCollection.get(position).getProductDisplayName());
-                b.putString(CommonWebviewFragment.URL_KEY,mContext.getString(R.string.server_endpoint)+commerceItemsesCollection.get(position).getProductPageUrl()+"&review=write");
-                msg.setData(b);
+            holder.mEditItemLayout.setVisibility(View.VISIBLE);
+            holder.mEditItemLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
-                try {
-                    mHandler.sendMessage(msg);
-                } catch (ClassCastException e) {
-                    e.printStackTrace();
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    Message msg = Message.obtain(null, Constants.CLICK_ITEM_UPDATE_SHOPPINGCART);
+                    Bundle b = new Bundle();
+                    b.putString(CommonWebviewFragment.TITLE_KEY, mCommerceItemsesCollection.get(position).getProductDisplayName());
+                    b.putString(CommonWebviewFragment.URL_KEY, mContext.getString(R.string.server_endpoint) + mCommerceItemsesCollection.get(position).getProductPageUrl() + "&review=write");
+                    msg.setData(b);
+
+                    try {
+                        mHandler.sendMessage(msg);
+                    } catch (ClassCastException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
+
+            });
+        } else {
+            holder.mEditItemLayout.setVisibility(View.GONE);
+        }
+
+        if (null != mCommerceItemsesCollection.get(position) && !mCommerceItemsesCollection.get(position).getQuantity().isEmpty()){
+            if (Integer.parseInt(mCommerceItemsesCollection.get(position).getQuantity()) > 0 && Integer.parseInt(mCommerceItemsesCollection.get(position).getQuantity()) < 10){
+                holder.mItemQuantityDescription.setVisibility(View.GONE);
+                holder.mItemQuantitySpinner.setVisibility(View.VISIBLE);
+
+                ArrayAdapter dataAdapter = new ArrayAdapter(mContext, android.R.layout.simple_spinner_item, mContext.getResources().getStringArray(R.array.items_quantity_array));
+                dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                holder.mItemQuantitySpinner.setAdapter(dataAdapter);
+                holder.mItemQuantitySpinner.setSelection((Integer.parseInt(mCommerceItemsesCollection.get(position).getQuantity()))-1);
+
+                holder.mItemQuantitySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+
+                        if (pos == 9 && parent.getItemAtPosition(pos).toString().equalsIgnoreCase("10+")){
+                            holder.mItemQuantityDescription.setVisibility(View.VISIBLE);
+                            holder.mItemQuantitySpinner.setVisibility(View.GONE);
+                            holder.mItemQuantityDescription.getEditText().setText(mCommerceItemsesCollection.get(position).getQuantity());
+                            holder.mItemQuantityDescription.requestFocus();
+                            InputMethodManager imm = (InputMethodManager) mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+                            imm.showSoftInput(holder.mItemQuantityDescription.getEditText(), InputMethodManager.SHOW_FORCED);
+
+                            holder.mItemQuantityDescription.getEditText().setOnKeyListener(new View.OnKeyListener() {
+                                @Override
+                                public boolean onKey(View v, int keyCode, KeyEvent event) {
+                                    if (((((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) || keyCode == EditorInfo.IME_ACTION_DONE) && (holder.mItemQuantityDescription.getEditText()).getText() != null) ) {
+                                        Message msg = Message.obtain(null,Constants.UPDATE_ITEM_QUANTITY_SHOPPINGCART);
+                                        Bundle b = new Bundle();
+
+                                        HashMap<String,String> commerceID_QuantityMap = new HashMap<>();
+
+                                        for (int i = 0 ; i < mCommerceItemsesCollection.size() ; i++){
+                                            commerceID_QuantityMap.put(mCommerceItemsesCollection.get(i).getCommerceItemId(),(mCommerceItemsesCollection.get(i).getQuantity()));
+                                        }
+                                        commerceID_QuantityMap.put(mCommerceItemsesCollection.get(position).getCommerceItemId(),holder.mItemQuantityDescription.getEditText().getText().toString().trim());
+                                        b.putSerializable(Constants.QUANTITY_MAP,commerceID_QuantityMap);
+                                        msg.setData(b);
+
+                                        try {
+                                            mHandler.sendMessage(msg);
+                                        } catch (ClassCastException e) {
+                                            e.printStackTrace();
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        } finally {
+                                            return true;
+                                        }
+                                    } else {
+                                        return false;
+                                    }
+                                }});
+                        }
+                        else if (!parent.getItemAtPosition(pos).toString().trim().equalsIgnoreCase(mCommerceItemsesCollection.get(position).getQuantity())) {
+                            Message msg = Message.obtain(null,Constants.UPDATE_ITEM_QUANTITY_SHOPPINGCART);
+                            Bundle b = new Bundle();
+
+                            HashMap<String,String> commerceID_QuantityMap = new HashMap<>();
+
+                            for (int i = 0 ; i < mCommerceItemsesCollection.size() ; i++){
+                                commerceID_QuantityMap.put(mCommerceItemsesCollection.get(i).getCommerceItemId(),(mCommerceItemsesCollection.get(i).getQuantity()));
+                            }
+                            commerceID_QuantityMap.put(mCommerceItemsesCollection.get(position).getCommerceItemId(),parent.getItemAtPosition(pos).toString().trim());
+                            b.putSerializable(Constants.QUANTITY_MAP,commerceID_QuantityMap);
+                            msg.setData(b);
+
+                            try {
+                                mHandler.sendMessage(msg);
+                            } catch (ClassCastException e) {
+                                e.printStackTrace();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
             }
-        });
+            else if (Integer.parseInt(mCommerceItemsesCollection.get(position).getQuantity()) >= 10){
+                holder.mItemQuantityDescription.setVisibility(View.VISIBLE);
+                holder.mItemQuantitySpinner.setVisibility(View.GONE);
+                holder.mItemQuantityDescription.getEditText().setText(mCommerceItemsesCollection.get(position).getQuantity());
+
+                holder.mItemQuantityDescription.getEditText().setOnKeyListener(new View.OnKeyListener() {
+                    @Override
+                    public boolean onKey(View v, int keyCode, KeyEvent event) {
+                        if (((((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) || keyCode == EditorInfo.IME_ACTION_DONE) && (holder.mItemQuantityDescription.getEditText()).getText() != null) ) {
+                            Message msg = Message.obtain(null,Constants.UPDATE_ITEM_QUANTITY_SHOPPINGCART);
+                            Bundle b = new Bundle();
+
+                            HashMap<String,String> commerceID_QuantityMap = new HashMap<>();
+
+                            for (int i = 0 ; i < mCommerceItemsesCollection.size() ; i++){
+                                commerceID_QuantityMap.put(mCommerceItemsesCollection.get(i).getCommerceItemId(),(mCommerceItemsesCollection.get(i).getQuantity()));
+                            }
+                            commerceID_QuantityMap.put(mCommerceItemsesCollection.get(position).getCommerceItemId(),holder.mItemQuantityDescription.getEditText().getText().toString().trim());
+                            b.putSerializable(Constants.QUANTITY_MAP,commerceID_QuantityMap);
+                            msg.setData(b);
+
+                            try {
+                                mHandler.sendMessage(msg);
+                            } catch (ClassCastException e) {
+                                e.printStackTrace();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            } finally {
+                                return true;
+                            }
+                        } else {
+                            return false;
+                        }
+                }});
+
+            } else {
+                holder.mItemQuantityDescription.setVisibility(View.VISIBLE);
+                holder.mItemQuantitySpinner.setVisibility(View.GONE);
+                holder.mItemQuantityDescription.getEditText().setText(mContext.getResources().getInteger(R.integer.default_Quantity));
+
+                holder.mItemQuantityDescription.getEditText().setOnKeyListener(new View.OnKeyListener() {
+                    @Override
+                    public boolean onKey(View v, int keyCode, KeyEvent event) {
+                        if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER) && (holder.mItemQuantityDescription.getEditText()).getText() != null ) {
+                            Message msg = Message.obtain(null,Constants.UPDATE_ITEM_QUANTITY_SHOPPINGCART);
+                            Bundle b = new Bundle();
+
+                            HashMap<String,String> commerceID_QuantityMap = new HashMap<>();
+
+                            for (int i = 0 ; i < mCommerceItemsesCollection.size() ; i++){
+                                commerceID_QuantityMap.put(mCommerceItemsesCollection.get(i).getCommerceItemId(),(mCommerceItemsesCollection.get(i).getQuantity()));
+                            }
+                            commerceID_QuantityMap.put(mCommerceItemsesCollection.get(position).getCommerceItemId(),holder.mItemQuantityDescription.getEditText().getText().toString().trim());
+                            b.putSerializable(Constants.QUANTITY_MAP,commerceID_QuantityMap);
+                            msg.setData(b);
+
+                            try {
+                                mHandler.sendMessage(msg);
+                            } catch (ClassCastException e) {
+                                e.printStackTrace();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            } finally {
+                                return true;
+                            }
+                        } else {
+                            return false;
+                        }
+                    }});
+            }
+        }
     }
 
     @Override
     public int getItemCount() {
-        return commerceItemsesCollection.size()+1;
+        return mCommerceItemsesCollection.size()+1;
     }
 
     public boolean isFooter(int position) {
-        return position == commerceItemsesCollection.size();
+        return position == mCommerceItemsesCollection.size();
     }
-
 
     @Override
     public int getItemViewType(int position) {
