c3b92000ada5ae32c4bce18f17f62fb8fd6c2719
dgirase <dgirase@dminc.com>
2016-11-23 17:49:28 +0530

    reverted PET-727
    Change-Id: Iec124d941ed5890f68458d6463281bc4655c855b

--- app/src/main/java/com/petmeds1800/ui/CommonWebViewActivity.java
+++ app/src/main/java/com/petmeds1800/ui/CommonWebViewActivity.java
@@ -3,6 +3,7 @@ package com.petmeds1800.ui;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.MenuItem;
 
 import com.petmeds1800.R;
 import com.petmeds1800.ui.fragments.CommonWebviewFragment;
@@ -28,5 +29,13 @@ public class CommonWebViewActivity extends AbstractActivity {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.container_web_view_fragment, webViewFragment);
         transaction.commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
