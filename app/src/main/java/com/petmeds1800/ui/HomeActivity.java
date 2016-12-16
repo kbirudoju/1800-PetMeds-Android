19036718dbd43faa29eba0741a68b376f876947e
dgirase <dgirase@dminc.com>
2016-12-16 18:31:21 +0530

    PETU-249: resolved
    Change-Id: I4f25e3606576aef3c5204e8a1660e60c3de88b14

--- app/src/main/java/com/petmeds1800/ui/HomeActivity.java
+++ app/src/main/java/com/petmeds1800/ui/HomeActivity.java
@@ -355,8 +355,6 @@ public class HomeActivity extends AbstractActivity
             mPreferencesHelper.setIsFingerPrintEnabled(false);
         } else if (!RxFingerprint.hasEnrolledFingerprints(this)) {
             mPreferencesHelper.setIsFingerPrintEnabled(false);
-        } else {
-            mPreferencesHelper.setIsFingerPrintEnabled(true);
         }
         if (getIntent().getBooleanExtra(IS_FROM_HOME_ACTIVITY, false) && mPreferencesHelper.getIsUserLoggedIn()) {
             showPushPermissionDailog();
