24416cfa9c8cbb131d93cb1f05868ef159561c69
psingh-dminc-com <psingh@dminc.com>
2017-01-25 16:05:28 +0530

    PETU:523->Andriod Need Stage Enviorment

---
+++ app/src/stage/java/com/petmeds1800/PetMedsApplication.java
@@ -0,0 +1,34 @@
+package com.petmeds1800;
+
+import com.petmeds1800.dagger.component.AppComponent;
+import com.petmeds1800.dagger.component.DaggerAppComponent;
+import com.petmeds1800.dagger.module.ApplicationModule;
+import com.petmeds1800.dagger.module.RestModule;
+import com.petmeds1800.dagger.module.StorageModule;
+import com.testfairy.TestFairy;
+
+public class PetMedsApplication extends PetMedsBaseApplication {
+
+    private static AppComponent mAppComponent;
+    public static boolean menuItemsClicked = false;
+    public static final String requestCredential="pmdevrestapi";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        TestFairy.begin(this, getString(R.string.testfairy_app_token));
+        mAppComponent = createAppComponent();
+
+    }
+    @Override
+    protected AppComponent createAppComponent() {
+        return DaggerAppComponent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .restModule(new RestModule(getString(R.string.server_endpoint), getApplicationContext()))
+                .storageModule(new StorageModule())
+                .build();
+    }
+    public  static synchronized AppComponent getAppComponent() {
+        return mAppComponent;
+    }
+}
